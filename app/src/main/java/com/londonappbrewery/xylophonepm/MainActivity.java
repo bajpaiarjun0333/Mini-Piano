9016fe2c1002465089151aff804482ca32b8f7c3
Arjun Bajpai <bajpaiarjun0333@gmail.com>
2019-01-01 22:28:51 +0530

    Complete App with every key ringing

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private int mGSoundId;
     private int mASoundId;
     private int mBSoundId;
+    SoundPool mSoundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +33,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
+            mSoundPool= new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS,AudioManager.STREAM_MUSIC,0);
 
 
         // TODO: Load and get the IDs to identify the sounds
-
-
+        mCSoundId=mSoundPool.load(getApplicationContext(),R.raw.note1_c,1);
+        mDSoundId=mSoundPool.load(getApplicationContext(),R.raw.note2_d,2);
+        mESoundId=mSoundPool.load(getApplicationContext(),R.raw.note3_e,3);
+        mFSoundId=mSoundPool.load(getApplicationContext(),R.raw.note4_f,4);
+        mGSoundId=mSoundPool.load(getApplicationContext(),R.raw.note5_g,5);
+        mASoundId=mSoundPool.load(getApplicationContext(),R.raw.note6_a,6);
+        mBSoundId=mSoundPool.load(getApplicationContext(),R.raw.note7_b,7);
     }
 
     // TODO: Add the play methods triggered by the buttons
+    public void playC(View v){
+        Log.d("Xylophone","Red Button Clicked");
+        mSoundPool.play(mCSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
+    public void playD(View v){
+        mSoundPool.play(mDSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
 
+    }
+    public void playE(View v){
+        mSoundPool.play(mESoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
 
+    }
+    public void playF(View v){
+        mSoundPool.play(mFSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
 
+    }
+    public void playG(View v){
+        mSoundPool.play(mGSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
+    public void playA(View v){
+        mSoundPool.play(mASoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
+    public void playB(View v){
+        mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+    }
 }
